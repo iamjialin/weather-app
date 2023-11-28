22d66674b8287e71a7a1a4a5fd16c4652e45e8d4
Charlie Yuan <brookisnothere@outlook.com>
2023-11-28 13:29:38 +1100

    deploy to vercel

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,13 @@
 import './App.css';
 import WeatherCard from 'components/WeatherCard';
 import BackgroundImage from 'components/BackgroundImage';
-import backgroundImage from './assets/backgrounds/background.png'
+import backgroundImage from './assets/backgrounds/bg.png'
 
 
 function App() {
   return(
 
-    <BackgroundImage className={"bg-no-repeat bg-right-bottom w-[1440px] mx-auto py-36 px-28"} src={backgroundImage} linearGradient={"linear-gradient(to bottom right, #89A2F0, #6558DF)"} >
+    <BackgroundImage className={"bg-no-repeat bg-right-bottom w-[1440px] mx-auto py-36 px-28"} src={backgroundImage.src} linearGradient={"linear-gradient(to bottom right, #89A2F0, #6558DF)"} >
       <WeatherCard />
     </BackgroundImage>
 
